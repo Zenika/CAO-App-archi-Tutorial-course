f5071d857f7e075c269bb2a101d8858a6a5138d1
Vassili <vassili.bernard@imie.fr>
2019-10-15 14:32:11 +0200

    init structure project

---
+++ adopteVaisseau/src/main/java/fr/space/adopteVaisseau/AdopteVaisseauApplication.java
@@ -0,0 +1,13 @@
+package fr.space.adopteVaisseau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopteVaisseauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopteVaisseauApplication.class, args);
+	}
+
+}
