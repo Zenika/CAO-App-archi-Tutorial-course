b3f9fd741e5d37c632e603fe6413b7d4743ff80b
Gueho Frederic <frederic.gueho@imie.fr>
2019-10-15 14:48:21 +0200

    Ajout package projet

---
+++ UnicornArtifact/src/main/java/UnicornProject/UnicornArtifact/App.java
@@ -0,0 +1,13 @@
+package UnicornProject.UnicornArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
