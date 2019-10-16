4736f39dec3f22dc18ee27e9872922ca91a53e09
Anatole <anatole.piveteau@imie.fr>
2019-10-16 11:38:56 +0200

    Zoning

--- sealion/public/js/global.js
+++ sealion/public/js/global.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     $("main>.nano").nanoScroller();
-})
+
+    $("#scrollToTop>a").click(function(event) {
+        event.preventDefault();
+        $("main>.nano>.nano-content").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 1000);
+    });
+});
--- sealion/resources/views/includes/scripts.blade.php
+++ sealion/resources/views/includes/scripts.blade.php
@@ -1,3 +1,2 @@
-<div id="scrollToTop"><a href="" title="Se rendre en haut de page"><i class="material-icons">chevron_left</i></a></div>
 <script src="{{url('/js/nano.js')}}"></script>
 <script src="{{url('/js/global.js')}}"></script>
