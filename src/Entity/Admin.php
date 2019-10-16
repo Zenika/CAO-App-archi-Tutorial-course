c088a120ea92d449611b6f7f2d5fcd14f9142bf2
paulpil <paulpil@hotmail.fr>
2019-10-16 12:10:56 +0200

    Add DB

---
+++ src/Entity/Admin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdminRepository")
+ */
+class Admin
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $AdminPseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAdminPseudo(): ?string
+    {
+        return $this->AdminPseudo;
+    }
+
+    public function setAdminPseudo(string $AdminPseudo): self
+    {
+        $this->AdminPseudo = $AdminPseudo;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Customers.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CustomersRepository")
+ */
+class Customers
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+
+    private $pseudo;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $FirstName;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $BirthDate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $SignInDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Adress;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $PhoneNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $Id): self
+    {
+        $this->Id = $Id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->BirthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $BirthDate): self
+    {
+        $this->BirthDate = $BirthDate;
+
+        return $this;
+    }
+
+    public function getSignInDate(): ?\DateTimeInterface
+    {
+        return $this->SignInDate;
+    }
+
+    public function setSignInDate(\DateTimeInterface $SignInDate): self
+    {
+        $this->SignInDate = $SignInDate;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): self
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->Adress;
+    }
+
+    public function setAdress(string $Adress): self
+    {
+        $this->Adress = $Adress;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->PhoneNumber;
+    }
+
+    public function setPhoneNumber(string $PhoneNumber): self
+    {
+        $this->PhoneNumber = $PhoneNumber;
+
+        return $this;
+    }
+}
