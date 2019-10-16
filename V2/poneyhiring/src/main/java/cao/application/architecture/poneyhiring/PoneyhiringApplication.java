c3a4ec3d749684581e73c6bacbb8fe2fc5a1170a
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-10-16 10:08:38 +0200

    Initialize poneyhiring

---
+++ V2/poneyhiring/src/main/java/cao/application/architecture/poneyhiring/PoneyhiringApplication.java
@@ -0,0 +1,13 @@
+package cao.application.architecture.poneyhiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoneyhiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoneyhiringApplication.class, args);
+	}
+
+}
---
+++ V2/poneyhiring/src/main/java/cao/application/architecture/poneyhiring/model/Client.java
@@ -0,0 +1,45 @@
+package cao.application.architecture.poneyhiring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Long id;
+    public String firstName;
+    public String lastName;
+
+    public Client() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) &&
+                Objects.equals(firstName, client.firstName) &&
+                Objects.equals(lastName, client.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+}
