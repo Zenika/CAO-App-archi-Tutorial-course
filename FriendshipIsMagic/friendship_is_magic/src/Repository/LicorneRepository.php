4c9a4b07c82da1c85736c7260044c0a8b19ad80f
Nicola Mascolo <n.mascolopro@gmail.com>
2019-10-16 10:26:42 +0200

    Adding entities and migration script to the project

---
+++ FriendshipIsMagic/friendship_is_magic/src/Repository/LicorneRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Licorne;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Licorne|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Licorne|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Licorne[]    findAll()
+ * @method Licorne[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LicorneRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Licorne::class);
+    }
+
+    // /**
+    //  * @return Licorne[] Returns an array of Licorne objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Licorne
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
