6589de7f0eb02062ff59b71b46889fe92ab75722
JohnnyYVARD <johnnyyvard@me.com>
2019-10-17 09:48:57 +0200

    Suppression de V2

--- V2/poneyhiring/src/main/java/cao/application/architecture/poneyhiring/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package cao.application.architecture.poneyhiring.repository;
-
-import cao.application.architecture.poneyhiring.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-
-}
