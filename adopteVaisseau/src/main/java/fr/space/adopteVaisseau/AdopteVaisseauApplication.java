03e099603571d17a1b5a4d10d62bc5976eef8370
Vassili <vassili.bernard@imie.fr>
2019-10-17 09:54:49 +0200

    add diagram class v2

--- adopteVaisseau/src/main/java/fr/space/adopteVaisseau/AdopteVaisseauApplication.java
+++ adopteVaisseau/src/main/java/fr/space/adopteVaisseau/AdopteVaisseauApplication.java
@@ -1,9 +1,10 @@
 package fr.space.adopteVaisseau;
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
-		import org.springframework.boot.builder.SpringApplicationBuilder;
-		import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-		import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdopteVaisseauApplication extends SpringBootServletInitializer {
@@ -12,4 +13,12 @@ public class AdopteVaisseauApplication extends SpringBootServletInitializer {
 		SpringApplication.run(AdopteVaisseauApplication.class, args);
 	}
 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
--- adopteVaisseau/src/main/java/fr/space/adopteVaisseau/ws/WsClient.java
+++ adopteVaisseau/src/main/java/fr/space/adopteVaisseau/ws/WsClient.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
 public class WsClient {
 
     private Logger logger = LoggerFactory.getLogger(WsClient.class);
